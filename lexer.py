ef8c0f2d4b18d7793ca245b8373d1d9642259bce
SujayKarpur <PES1UG23CS607@pesu.pes.edu>
2025-01-02 12:09:21 +0000

    finish lexer

---
+++ lexer.py
@@ -0,0 +1,17 @@
+import errors 
+import tokens 
+
+
+def lex(string, verbose=True):
+    lexer_position = 0 
+    lexer_row = lexer_column = 0 
+    lexed_tokens = [] 
+    max_position = len(string)
+    while lexer_position < max_position:
+        new_token, forward_position = tokens.TokenType.match(string, lexer_position, lexer_row, lexer_column)
+        lexer_position += forward_position
+        lexed_tokens.append(new_token)
+    if not verbose: 
+        ignored_tokens = {tokens.TokenType.COMMENT.name, tokens.TokenType.MULTI_LINE_COMMENT.name, tokens.TokenType.SPACE.name, tokens.TokenType.NEWLINE.name, tokens.TokenType.TAB.name}
+        lexed_tokens = list(filter(lambda i : i.type not in ignored_tokens, lexed_tokens)) 
+    return lexed_tokens
