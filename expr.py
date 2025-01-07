8fa20ce1fd17bd8aa372480eb0c19bd52bc7e19d
SujayKarpur <PES1UG23CS607@pesu.pes.edu>
2025-01-07 14:08:08 +0000

    define visitor protocol

--- expr.py
+++ expr.py
@@ -5,13 +5,29 @@ from typing import Any, Protocol, TypeVar
 from tokens import Token 
 
 
+R = TypeVar('R') #Generic 
 
+
+
+class Visitor(Protocol[R]): 
+    
+    def visit_binary_expression(self, expr : 'Binary') -> R: #forward references
+        pass 
+
+    def visit_grouping_expression(self, expr : 'Grouping') -> R:
+        pass  
+
+    def visit_literal_expression(self, expr : 'Literal') -> R:
+        pass  
+
+    def visit_unary_expression(self, expr : 'Unary') -> R:
+        pass  
 
 
 class Expr(ABC): 
     
     @abstractmethod
-    def accept():
+    def accept(self, visitor: Visitor[R]) -> R:
         pass  
 
 @dataclass
@@ -20,15 +36,27 @@ class Binary(Expr):
     operator : Token 
     right : Expr  
 
+    def accept(self, visitor: Visitor[R]) -> R:
+        visitor.visit_binary_expression(self)
+
 @dataclass
 class Grouping(Expr):
     expression : Expr 
+
+    def accept(self, visitor: Visitor[R]) -> R:
+        visitor.visit_grouping_expression(self)
 
 @dataclass
 class Literal(Expr):
     value : Any  
 
+    def accept(self, visitor: Visitor[R]) -> R:
+        visitor.visit_literal_expression(self)
+
 @dataclass
 class Unary(Expr):
     operator : Token 
     right : Expr  
+
+    def accept(self, visitor: Visitor[R]) -> R:
+        visitor.visit_unary_expression(self)
