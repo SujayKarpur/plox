f1aa8113e7a90c6319b63e81c889977cb2f7e504
SujayKarpur <PES1UG23CS607@pesu.pes.edu>
2025-01-08 12:55:13 +0000

    Remove duplicate files

--- errors.py
+++
@@ -1,9 +0,0 @@
-error_flag = False 
-
-def report(line, where, message):
-    print("[line " + str(line+1) + "] Error" + where + ": " + message)
-    global error_flag 
-    error_flag = True 
-
-def error(line, message):
-    report(line, "", message)
