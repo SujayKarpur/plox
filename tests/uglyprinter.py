db03fa5f7101226ab881beaa51113e473553fa05
SujayKarpur <PES1UG23CS607@pesu.pes.edu>
2025-01-08 13:31:30 +0000

    Fix issue in AST printer

--- tests/uglyprinter.py
+++ tests/uglyprinter.py
@@ -6,13 +6,13 @@ from lox.expr import Visitor, Binary, Grouping, Literal, Unary
 class Printer(Visitor[str]):
 
     def visit_binary_expression(self, expr : Binary) -> str: 
-        return f"({expr.operator} {expr.left.accept(self, expr.left)} {expr.right.accept(self, expr.right)})" 
+        return f"({expr.operator} {expr.left.accept(self)} {expr.right.accept(self)})" 
 
     def visit_grouping_expression(self, expr : Grouping) -> str:
-        return f"('group', {expr.expression.accept(self, expr.expression)})" 
+        return f"('group', {expr.expression.accept(self)})" 
 
     def visit_literal_expression(self, expr : Literal) -> str:
         return f"({expr.value})"   
 
     def visit_unary_expression(self, expr : Unary) -> str:
-        return f"({expr.operator} {expr.right.accept(self, expr.right)})"   
+        return f"({expr.operator} {expr.right.accept(self)})"   
